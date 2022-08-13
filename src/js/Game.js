de13ee9b400bb137b5ada21150f1f636ea070263
Austin Wasinger <austinwasinger@gmail.com>
2022-08-13 15:37:18 -0600

    loaded image

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,4 +1,5 @@
 import { Tilemap } from './Tilemap.js';
+import tileset from '@/assets/tiles/dungeonTileset.png';
 const createjs = window.createjs;
 
 export function init() {
@@ -9,10 +10,33 @@ export function init() {
 
   const circle = new createjs.Shape();
   circle.graphics.beginFill('DeepSkyBlue').drawCircle(0, 0, 50);
-  circle.x = 100;
-  circle.y = 100;
+  circle.x = 500;
+  circle.y = 500;
   stage.addChild(circle);
   stage.update();
+  loadImage(stage);
+
+  window.requestAnimationFrame((dt) => loop(dt, stage));
+}
+
+function loop(dt, stage) {
+  stage.update();
+  window.requestAnimationFrame((dt) => loop(dt, stage));
+}
+
+function loadImage(stage) {
+  const preload = new createjs.LoadQueue();
+  preload.addEventListener('fileload', (event) => handleFileComplete(event, stage));
+  preload.loadFile(tileset);
+}
+
+function handleFileComplete(event, stage) {
+  const bmp = new createjs.Bitmap(event.result);
+  bmp.x = 50;
+  bmp.y = 50;
+  stage.addChild(bmp);
+  stage.update();
+  console.log('image loaded');
 }
 
 function resize(canvas) {
